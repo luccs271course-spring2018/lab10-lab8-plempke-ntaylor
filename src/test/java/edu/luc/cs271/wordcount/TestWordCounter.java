ce66fc742be2384433084095b1681c2bcc513c6c
Philip Lempke <plempke@luc.edu>
2017-11-07 19:27:31 +0000

    TestWordCounter worked on
    Signed-off-by: Philip Lempke <plempke@luc.edu>

--- src/test/java/edu/luc/cs271/wordcount/TestWordCounter.java
+++ src/test/java/edu/luc/cs271/wordcount/TestWordCounter.java
@@ -5,29 +5,41 @@ import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-
+import java.util.*;
 
 public class TestWordCounter {
 
   // TODO complete this test class
-
+  // DONE
+  
+  
   // TODO declare a reference to the SUT (system under test), i.e., WordCounter
+  // DONE
+  private Map<String, Integer> tempMap;
+  WordCounter count = new WordCounter(tempMap);
+  
 
   @Before
   public void setUp() {
     // TODO create the SUT instance
+    // DONE
+    tempMap = new HashMap<>();
   }
 
   @After
   public void tearDown() {
     // TODO set the SUT instance to null
+    // DONE
+    tempMap = null;
   }
 
   @Test
   public void testGetCountEmpty() {
 
     // TODO verify that the SUT initially returns an empty map
-    fail();
+    // DONE
+    assertTrue(tempMap.isEmpty());
+
 
   }
 
@@ -37,7 +49,14 @@ public class TestWordCounter {
     // TODO run the SUT on a specific String iterator with some repeated words,
     // then use assertions to verify the correct counts
     // do this for at least two words in the iterator and two not in the iterator
-    fail();
+    
+    // DONE, BUT CHECK LATER
+    
+    Iterator someIterator = Arrays.asList("asdf", "oiu", "qwer").iterator()
+    count.countWords(someIterator);
+    assertNotEquals(2, object.getCount("asdf"));
+    assertNotEquals(2, object.getCount("oiu"));
+    assertNotEquals(2, object.getCount("qwer"));
 
   }
 }
