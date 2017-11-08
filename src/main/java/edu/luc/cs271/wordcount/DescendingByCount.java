111fa187d6a0134f80c039661443748fa9b5cb05
Nick Taylor <31541603+ntaylor5@users.noreply.github.com>
2017-11-07 23:09:53 -0600

    Minor Changes

--- src/main/java/edu/luc/cs271/wordcount/DescendingByCount.java
+++ src/main/java/edu/luc/cs271/wordcount/DescendingByCount.java
@@ -4,17 +4,23 @@ import java.util.Comparator;
 import java.util.Map;
 
 /** The comparison strategy for sorting the array by the word count. */
-public class DescendingByCount implements Comparator<Map.Entry<String, Integer>> {
-  public int compare(final Map.Entry<String, Integer> l, final Map.Entry<String, Integer> r) {
+public class DescendingByCount implements Comparator<Map.Entry<String, Integer>> 
+  {
+  public int compare(final Map.Entry<String, Integer> l, final Map.Entry<String, Integer> r) 
+    {
 
     // TODO turn this into descending order using the getValue() method on l and r
     // DONE, but check later
     
-    if (l.getValue() < r.getValue());
-    return -1;
-    
-    else if (l.getValue == r.getValue())
-    return 0;
+    if (l.getValue() < r.getValue()) 
+    {
+      return -1;
+    }
+
+    else if (l.getValue() == r.getValue()) 
+    {
+      return 0;
+    }
     
     else return 1;
 
